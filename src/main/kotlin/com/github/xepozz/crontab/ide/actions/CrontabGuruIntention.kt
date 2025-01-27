56c107fbd64cdcc7659d01c1fcea71bde77d175b
Dmitrii Derepko <xepozz@list.ru>
2025-01-27 16:20:19 +0300

    rewrite intention text

--- src/main/kotlin/com/github/xepozz/crontab/ide/actions/CrontabGuruIntention.kt
+++ src/main/kotlin/com/github/xepozz/crontab/ide/actions/CrontabGuruIntention.kt
@@ -4,15 +4,18 @@ import com.github.xepozz.crontab.CrontabIcons
 import com.github.xepozz.crontab.ide.CrontabGuruUtils
 import com.github.xepozz.crontab.language.psi.CrontabPsiTreeUtils
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.codeInsight.intention.preview.IntentionPreviewInfo
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Iconable
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 
-class CrontabGuruIntention : PsiElementBaseIntentionAction(), Iconable {
+class CrontabGuruIntention : PsiElementBaseIntentionAction(), Iconable, DumbAware {
     override fun invoke(
         project: Project,
-        editor: Editor,
+        editor: Editor?,
         element: PsiElement
     ) {
         val crontabSchedule = CrontabPsiTreeUtils.findCrontabSchedule(element)
@@ -22,7 +25,9 @@ class CrontabGuruIntention : PsiElementBaseIntentionAction(), Iconable {
         }
     }
 
-    override fun isAvailable(project: Project, editor: Editor, element: PsiElement) = true
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement) = true
+
+    override fun generatePreview(project: Project, editor: Editor, file: PsiFile) = IntentionPreviewInfo.EMPTY
 
     override fun getFamilyName() = "Crontab intentions"
 
